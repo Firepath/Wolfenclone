bf5ae2ce7a48f609337346782d6650f0d06d2b5a
Brad Ward <kingbeest@hotmail.com>
2018-07-14 13:12:57 +1000

    Added Font class for drawing text. This is basically copied from Sprite project but changed to take a pointer to the surface to use, and to take a colour to draw the font in.

---
+++ Engine/Font.cpp
@@ -0,0 +1,50 @@
+#include <assert.h>
+
+#include "Font.h"
+#include "SpriteEffect.h"
+
+Font::Font( Surface* const surface, Color colour, Color chroma )
+	:
+	Sprite( surface ),
+	GlyphWidth( Sprite->GetWidth() / Font::SpriteSheetColumns ),
+	GlyphHeight( Sprite->GetHeight() / Font::SpriteSheetRows ),
+	Chroma( chroma ),
+	Colour( colour )
+{
+	assert( GlyphWidth * Font::SpriteSheetColumns == Sprite->GetWidth() );
+	assert( GlyphHeight * Font::SpriteSheetRows == Sprite->GetHeight() );
+}
+
+void Font::DrawString( const std::string& text, const Vei2& location, Graphics& gfx ) const
+{
+	SpriteEffect::Sustitution effect( Chroma, Colour );
+	Vei2 currentLocation = location;
+	for ( auto c : text )
+	{
+		if ( c == '\n' )
+		{
+			currentLocation.y += GlyphHeight;
+			currentLocation.x = location.x;
+			continue;
+		}
+
+		if ( c >= Font::FirstChar + 1 && c <= Font::LastChar )
+		{
+			gfx.DrawSprite( currentLocation.x, currentLocation.y, MapGlyphRect( c ), Graphics::GetScreenRect(), *Sprite, effect );
+		}
+
+		currentLocation.x += GlyphWidth;
+	}
+}
+
+RectI Font::MapGlyphRect( char c ) const
+{
+	assert( c >= Font::FirstChar && c <= Font::LastChar );
+
+	const int glyphIndex = c - Font::FirstChar;
+
+	const int glyphY = glyphIndex / Font::SpriteSheetColumns;
+	const int glyphX = glyphIndex % Font::SpriteSheetColumns;
+
+	return RectI( { glyphX * GlyphWidth, glyphY * GlyphHeight }, GlyphWidth, GlyphHeight );
+}
---
+++ Engine/Font.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#define NOMINMAX
+
+#include <string>
+
+#include "Colors.h"
+#include "Graphics.h"
+#include "Rect.h"
+#include "Surface.h"
+#include "Vec2.h"
+
+class Font
+{
+public:
+	Font( Surface* const surface, Color colour, Color chroma = Colors::Magenta );
+
+	void DrawString( const std::string& text, const Vei2& location, Graphics& gfx ) const;
+
+private:
+	RectI MapGlyphRect( char c ) const;
+
+private:
+	static constexpr int SpriteSheetColumns = 32;
+	static constexpr int SpriteSheetRows = 3;
+	static constexpr char FirstChar = ' ';
+	static constexpr char LastChar = '~';
+
+	Surface* Sprite = nullptr;
+
+	int GlyphWidth;
+	int GlyphHeight;
+
+	Color Chroma;
+	Color Colour;
+};
