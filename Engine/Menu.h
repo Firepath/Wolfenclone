840eedc2a6eaf994ad1562a98100a5b28129fc43
Brad Ward <kingbeest@hotmail.com>
2018-07-15 07:50:46 +1000

    Fleshed out menu to draw bordered box and font on top.

--- Engine/Menu.h
+++ Engine/Menu.h
@@ -3,20 +3,121 @@
 #include <string>
 #include <vector>
 
+#include "Graphics.h"
+#include "Rect.h"
+#include "SpriteEffect.h"
+#include "Vec2.h"
+
+class SelectedCallBack
+{
+public:
+	~SelectedCallBack() = default;
+
+	virtual void Execute() const = 0;
+};
+
+class TestCallBack : public SelectedCallBack
+{
+public:
+	void Execute() const override
+	{
+		int i = 5;
+	}
+};
+
 class Menu
 {
 private:
+	static constexpr Color DefaultBorderColour = Colors::LightGray;
+	static constexpr size_t DefaultBorderThickness = 3u;
+	static constexpr Color DefaultBoxColour = Colors::Gray;
+	static constexpr size_t DefaultBoxPadding = 4u;
+	static constexpr size_t DefaultMaxHeight = 30u;
+	static constexpr size_t DefaultMaxWidth = 100u;
+	static constexpr float DefaultOpacity = 50.0f;
+
 	class MenuItem
 	{
+	public:
+
+		MenuItem( std::string text, std::unique_ptr<SelectedCallBack> callback )
+			:
+			Text( text ),
+			CallBack( std::move( callback ) )
+		{
+		}
+
+		const std::string GetText() const
+		{
+			return Text;
+		}
+
+		void SetText( const std::string text )
+		{
+			Text = text;
+		}
+
+		void Select()
+		{
+			CallBack->Execute();
+		}
+
+	private:
+		std::string Text;
+		std::unique_ptr<SelectedCallBack> CallBack;
 	};
 
 public:
-	template<typename SelectedCallBack>
-	void AddMenuItem( std::string text, SelectedCallBack callback )
+	Menu( const Font& font )
+		:
+		_Font( font )
 	{
-		//TODO: Implement AddMenuItem
+		BoxHeight = _Font.GetGlyphHeight() + 2 * BoxPadding;
+	}
+
+	void AddMenuItem( std::string text, std::unique_ptr<SelectedCallBack> callback )
+	{
+		MenuItems.emplace_back(  text, std::move( callback ) );
+	}
+
+	const Vei2 GetLocation()
+	{
+		return Location;
+	}
+
+	void SetLocation( const Vei2 screenLocation )
+	{
+		Location = screenLocation;
+	}
+
+	void Show( Graphics& gfx )
+	{
+		// TODO: Smarts to make sure menu shows on screen as best it can
+		// TODO: Further smarts to allow scrolling menu that is off the screen
+
+		SpriteEffect::Transparency boxEffect( Opacity );
+
+		Vei2 location = Location;
+		for ( auto it = MenuItems.begin(); it != MenuItems.end(); it++ )
+		{
+			gfx.DrawBorderedBox( RectI( location, BoxWidth, BoxHeight ), BoxColour, BorderColour, boxEffect, BorderThickness );
+			_Font.DrawString( it->GetText(), location + Vei2( (int)BoxPadding, (int)BoxPadding ), gfx );
+			location.y += BoxHeight;
+		}
 	}
 
 private:
+	size_t BoxHeight = Menu::DefaultMaxHeight;
+	size_t BoxWidth = Menu::DefaultMaxWidth;
+
+	Color BorderColour = Menu::DefaultBorderColour;
+	size_t BorderThickness = Menu::DefaultBorderThickness;
+	Color BoxColour = Menu::DefaultBoxColour;
+	size_t BoxPadding = Menu::DefaultBoxPadding;
+	float Opacity = Menu::DefaultOpacity;
+
+	Vei2 Location;
+	const Font& _Font;
+
 	std::vector<MenuItem> MenuItems;
 };
