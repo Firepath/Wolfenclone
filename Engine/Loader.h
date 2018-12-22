44cc9c1c3a887440a7410c3867d42fa1fb735d00
Brad Ward <kingbeest@hotmail.com>
2018-12-22 13:53:37 +1000

    Made protected methods private in Loader as they didn't need to be protected.

--- Engine/Loader.h
+++ Engine/Loader.h
@@ -13,10 +13,9 @@ public:
 protected:
 	LoadConstants::ReadMode Mode = LoadConstants::ReadMode::None;
 
-	virtual void InitialiseLoad();
-	virtual void ReadSetting( const std::string& line ) = 0;
-
 private:
 	void _Load( const std::string& filename );
 	virtual void FinaliseLoad();
+	virtual void InitialiseLoad();
+	virtual void ReadSetting( const std::string& line ) = 0;
 };
