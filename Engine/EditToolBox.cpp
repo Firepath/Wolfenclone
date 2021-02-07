17cdd9eb593ea033cd16be246a1d042f8afe0012
Brad Ward <kingbeest@hotmail.com>
2021-02-07 12:31:19 +1000

    Fixed broken null references doing things the way it used to be done...

--- Engine/EditToolBox.cpp
+++ Engine/EditToolBox.cpp
@@ -11,15 +11,24 @@ void EditToolBox::SetupTools( Editor* const editor )
 	std::unique_ptr<StringKeyRepository<EditTool>> mouseButtonTools = std::make_unique<StringKeyRepository<EditTool>>();
 
 	std::unique_ptr<EditTool> mouseButtonToolNone = std::make_unique<EditTool_MouseButton_None>( editor );
-	mouseButtonTools->AddItem( mouseButtonToolNone->GetTypeName(), std::move( mouseButtonToolNone ) );
+	auto name = mouseButtonToolNone->GetTypeName();
+	mouseButtonTools->AddItem( name, std::move( mouseButtonToolNone ) );
+
 	std::unique_ptr<EditTool> mouseButtonToolInsert = std::make_unique<EditTool_MouseButton_Insert>( editor, nullptr );
-	mouseButtonTools->AddItem( mouseButtonToolInsert->GetTypeName(), std::move( mouseButtonToolInsert ) );
+	name = mouseButtonToolInsert->GetTypeName();
+	mouseButtonTools->AddItem( name, std::move( mouseButtonToolInsert ) );
+
 	std::unique_ptr<EditTool> mouseButtonToolDelete = std::make_unique<EditTool_MouseButton_Delete>( editor );
-	mouseButtonTools->AddItem( mouseButtonToolDelete->GetTypeName(), std::move( mouseButtonToolDelete ) );
+	name = mouseButtonToolDelete->GetTypeName();
+	mouseButtonTools->AddItem( name, std::move( mouseButtonToolDelete ) );
+
 	std::unique_ptr<EditTool> mouseButtonToolSelect = std::make_unique<EditTool_MouseButton_Select>( editor );
-	mouseButtonTools->AddItem( mouseButtonToolSelect->GetTypeName(), std::move( mouseButtonToolSelect ) );
+	name = mouseButtonToolSelect->GetTypeName();
+	mouseButtonTools->AddItem( name, std::move( mouseButtonToolSelect ) );
+
 	std::unique_ptr<EditTool> mouseButtonToolMove = std::make_unique<EditTool_MouseButton_Move>( editor );
-	mouseButtonTools->AddItem( mouseButtonToolMove->GetTypeName(), std::move( mouseButtonToolMove ) );
+	name = mouseButtonToolMove->GetTypeName();
+	mouseButtonTools->AddItem( name, std::move( mouseButtonToolMove ) );
 
 	ToolRepository->AddItem( EditTool_MouseButton::TypeName, std::move( mouseButtonTools ) );
 }
